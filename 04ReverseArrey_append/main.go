5d47a257341828bb54e564a37496970b3317dafb
abhishe <abhishekkant051@gmail.com>
2026-01-17 11:16:13 +0530

    update

--- 04ReverseArrey_append/main.go
+++ 04ReverseArrey_append/main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	arrey:= []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	reversed := []int{}
 
-	for i := len(arrey) - 1; i >= 0; i-- {
-		reversed = append(reversed, arrey[i])
+	for i := len(arr) - 1; i >= 0; i-- {
+		reversed = append(reversed, arr[i])
 	}
-	fmt.Println("Orignal Number ",arrey)
-	fmt.Println("Reversed Number ",reversed)
+	fmt.Println("Orignal Number:", arr)
+	fmt.Println("Append Number is", reversed)
 }
