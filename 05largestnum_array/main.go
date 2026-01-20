4bbf2478b558e26ac97cbf867a48b29f45c9aa89
abhishe <abhishekkant051@gmail.com>
2026-01-20 09:18:26 +0530

    update

--- 05largestnum_array/main.go
+++ 05largestnum_array/main.go
@@ -1,17 +1,17 @@
 package main
 
+
 import"fmt"
+func main(){
+arr:=[]int{1,22,34,54,57,65,80}
 
-func main (){
-	arr:=[6]int{1,2,3,43,45,46}
-	max:=arr[0]
+max:=arr[0]
 
-	for i:=1;i<len(arr);i++{
-		if arr[i]>max {
-			max=arr[i]
-			
-		}
-
+for i:=1;i<len(arr);i++{
+	if arr[i]>max{
+		max=arr[i]
 	}
-	fmt.Println("LargestNum",max)
+}
+fmt.Println("Largest number is thr Array Is",max)
+
 }
