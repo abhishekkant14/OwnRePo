51415b0784fe510c7201e07b88fe1ad707d39d63
abhishe <abhishekkant051@gmail.com>
2026-01-20 19:49:06 +0530

    Largest Number Is

--- 05largestnum_array/main.go
+++ 05largestnum_array/main.go
@@ -1,17 +1,13 @@
 package main
 
-
 import"fmt"
 func main(){
-arr:=[]int{1,22,34,54,57,65,80}
-
-max:=arr[0]
-
-for i:=1;i<len(arr);i++{
-	if arr[i]>max{
-		max=arr[i]
+	array:=[]int{10,23,24,26,25,78,56,89}
+	Largest:=array[0]
+	for i:=1;i<len(array);i++{
+		if array[i]>Largest{
+			Largest=array[i]
+		}
 	}
-}
-fmt.Println("Largest number is thr Array Is",max)
-
+	fmt.Println(Largest)
 }
