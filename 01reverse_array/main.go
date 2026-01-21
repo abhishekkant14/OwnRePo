b882ea9bc6fc6fffc40bcef4eba55094564b3db9
abhishe <abhishekkant051@gmail.com>
2026-01-21 20:50:32 +0530

    Value In Go Lang

--- 01reverse_array/main.go
+++ 01reverse_array/main.go
@@ -1,14 +1,14 @@
-package main
+package main 
 
-import "fmt"
 
-func main() {
+import"fmt"
 
-	arr := [5]int{1, 2, 3, 4, 5}
-fmt.Println("Number",arr)
-	for i, a := 0, len(arr)-1; i < a; i, a = i+1, a-1 {
-		arr[i], arr[a] = arr[a], arr[i]
-	}
-	
-	fmt.Println("Reverse arr",arr)
+func main(){
+Arr:=[]int{1,2,3,4,5,6,7,8,9}
+
+for i,a:=0,len(Arr)-1;i<a;i,a=1+i,a-1{
+	Arr[i],Arr[a]=Arr[a],Arr[i]
+}
+fmt.Println("Reversed Arrar Number ",Arr)
+
 }
